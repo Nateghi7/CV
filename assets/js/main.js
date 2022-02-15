e76a46b36538bb833aca91938d26ebff726d7540
Nateghi7 <Nateghi7@gmail.com>
2022-02-15 12:51:32 -0800

    v0.0.3
    v0.0.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@ showMenu('nav-toggle','nav-menu')
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
+
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
